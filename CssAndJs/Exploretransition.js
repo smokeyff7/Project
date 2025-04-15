b7a923976712620a11163bbbd307184894e68054
smokeyff7 <132787854+smokeyff7@users.noreply.github.com>
2025-04-15 14:44:00 +0530

    Add files via upload

---
+++ CssAndJs/Exploretransition.js
@@ -0,0 +1,10 @@
+function scrollToNext() {
+    document.getElementById("next").scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+
+  function scrollToLanding() {
+    document.getElementById("landing").scrollIntoView({
+      behavior: "smooth"
+    });
